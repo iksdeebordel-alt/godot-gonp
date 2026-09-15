e192e8462682f381ab6aaccb777a6ab08fa00544
iksdeebordel-alt <iksdeebordel@gmail.com>
2026-09-15 21:55:07 +0200

    morecommitment

---
+++ entities/ball/ball.gd
@@ -0,0 +1,22 @@
+class_name Ball
+
+extends Area2D
+
+@export var speed: float = 600
+var velocity: Vector2 =  Vector2(1, 0)
+
+
+func _ready() -> void:
+	area_entered.connect(_on_paddle_hit) 
+
+
+func _physics_process(delta):
+	position.x += (velocity.x * delta * speed)
+	position.y += (velocity.y * delta * speed)
+
+
+
+func _on_paddle_hit(area: Area2D):
+	if area.is_in_group("paddle"):
+		print("Hit!");
+		velocity.x *= -1
